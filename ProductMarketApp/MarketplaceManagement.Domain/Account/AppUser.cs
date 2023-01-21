fba08458387ae3542f0bc2d3307e83f33c1ae46e
Albert <user@WINDOWS-O7N9M38>
2023-01-21 16:28:31 +0400

    add new project  marketplace domain and add app user

---
+++ ProductMarketApp/MarketplaceManagement.Domain/Account/AppUser.cs
@@ -0,0 +1,10 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+
+namespace MarketplaceManagement.Domain.Account;
+
+public class AppUser:IdentityUser
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}

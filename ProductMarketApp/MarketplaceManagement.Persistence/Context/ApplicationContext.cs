e20bfaaf4f3605fb688637940b5554bec6c9e074
Albert <user@WINDOWS-O7N9M38>
2023-01-21 16:29:20 +0400

    add persistence project and add context class

---
+++ ProductMarketApp/MarketplaceManagement.Persistence/Context/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using MarketplaceManagement.Domain.Account;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarketplaceManagement.Persistence.Context;
+
+public class ApplicationContext:IdentityDbContext<AppUser>
+{
+	public ApplicationContext(DbContextOptions<ApplicationContext> opt):base(opt)
+	{
+
+	}
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+}

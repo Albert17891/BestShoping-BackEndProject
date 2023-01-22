b64ecd594264f52278565d8e12cbe43f983a4ba3
Albert <user@WINDOWS-O7N9M38>
2023-01-22 15:39:27 +0400

    add IProductRepositroy and implement it

---
+++ ProductMarketApp/MarketplaceManagement.DataAccess/Abstractions/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using MarketplaceManagement.Domain.ProductModel;
+
+namespace MarketplaceManagement.DataAccess.Abstractions;
+
+public interface IProductRepository
+{
+    IQueryable<Product> Table { get; }
+
+    Task AddAsync(Product product);
+    void Update(Product product);
+}
---
+++ ProductMarketApp/MarketplaceManagement.DataAccess/Services/ProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using MarketplaceManagement.DataAccess.Abstractions;
+using MarketplaceManagement.Domain.ProductModel;
+using MarketplaceManagement.Persistence.Context;
+
+namespace MarketplaceManagement.DataAccess.Services;
+
+public class ProductRepository : BaseRepository<Product>, IProductRepository
+{
+	private readonly ApplicationContext _context;
+
+	public ProductRepository(ApplicationContext context) : base(context)
+	{
+		_context = context;
+	}
+
+	public async Task AddAsync(Product product)
+	{
+		await AddAsync(product);
+	}
+}

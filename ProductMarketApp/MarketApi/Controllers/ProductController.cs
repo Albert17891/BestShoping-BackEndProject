46fba73348dbdf924e7854031791c53a16f30e8f
Albert <user@WINDOWS-O7N9M38>
2023-01-22 15:08:48 +0400

    add ProductController

---
+++ ProductMarketApp/MarketApi/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Mapster;
+using MarketApi.Model.Request.Product;
+using MarketplaceManagement.ProductService.Abstractions;
+using MarketplaceManagement.ProductService.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketApi.Controllers;
+[Route("[controller]")]
+[ApiController]
+public class ProductController : ControllerBase
+{
+    private readonly IProductService _productService;
+
+    public ProductController(IProductService productService)
+    {
+        _productService = productService;
+    }
+
+    [Route("product-add")]
+    [HttpPost]
+    public async Task<IActionResult> ProductAdd(ProductRequest productRequest, CancellationToken cancellationToken = default)
+    {
+        if (productRequest is null)
+        {
+            return BadRequest();
+        }
+
+        await _productService.AddProductAsync(productRequest.Adapt<ProductServcieModel>(), cancellationToken);
+
+        return Ok();
+    }
+}

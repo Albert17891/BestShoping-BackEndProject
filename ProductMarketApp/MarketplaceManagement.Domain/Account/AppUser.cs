961dd73d9022b316ff95dfc7bb4d57e78dfa2e60
Albert <user@WINDOWS-O7N9M38>
2023-01-25 19:28:11 +0400

    Add Domain

---
+++ ProductMarketApp/MarketplaceManagement.Domain/Account/AppUser.cs
@@ -0,0 +1,13 @@
+﻿
+using MarketplaceManagement.Domain.ProductModel;
+using Microsoft.AspNetCore.Identity;
+
+namespace MarketplaceManagement.Domain.Account;
+
+public class AppUser:IdentityUser
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public List<UserProduct> UserProducts { get; set; }
+}
---
+++ ProductMarketApp/MarketplaceManagement.Domain/UserProduct.cs
@@ -0,0 +1,12 @@
+﻿using MarketplaceManagement.Domain.Account;
+using MarketplaceManagement.Domain.ProductModel;
+
+namespace MarketplaceManagement.Domain;
+
+public class UserProduct
+{
+    public string UserId { get; set; }
+    public int ProductId { get; set; }
+    public Product Product { get; set; }
+    public AppUser AppUser { get; set; }
+}

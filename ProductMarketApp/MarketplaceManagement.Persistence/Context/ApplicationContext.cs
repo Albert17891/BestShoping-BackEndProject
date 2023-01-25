62e51c8a08d8b5aa78b868b72111b3f54debc3c2
Albert <user@WINDOWS-O7N9M38>
2023-01-25 19:28:24 +0400

    add persistence

---
+++ ProductMarketApp/MarketplaceManagement.Persistence/Context/ApplicationContext.cs
@@ -0,0 +1,37 @@
+﻿using MarketplaceManagement.Domain;
+using MarketplaceManagement.Domain.Account;
+using MarketplaceManagement.Domain.ProductModel;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarketplaceManagement.Persistence.Context;
+
+public class ApplicationContext : IdentityDbContext<AppUser>
+{
+    public ApplicationContext(DbContextOptions<ApplicationContext> opt) : base(opt)
+    {
+
+    }
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<UserProduct> UserProducts { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<UserProduct>()
+            .HasKey(x => new { x.UserId, x.ProductId });
+
+        builder.Entity<UserProduct>()
+            .HasOne<Product>(x => x.Product)
+            .WithMany(x => x.UsersProducts)
+            .HasForeignKey(x=>x.ProductId);
+
+        builder.Entity<UserProduct>()
+            .HasOne<AppUser>(x => x.AppUser)
+            .WithMany(x => x.UserProducts)
+            .HasForeignKey(x=>x.UserId);
+            
+
+        base.OnModelCreating(builder);
+    }
+}
---
+++ ProductMarketApp/MarketplaceManagement.Persistence/Migrations/20230122105757_Add_Product_Entity.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MarketplaceManagement.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserProducts",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserProducts", x => new { x.UserId, x.ProductId });
+                    table.ForeignKey(
+                        name: "FK_UserProducts_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserProducts_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserProducts_ProductId",
+                table: "UserProducts",
+                column: "ProductId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserProducts");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
